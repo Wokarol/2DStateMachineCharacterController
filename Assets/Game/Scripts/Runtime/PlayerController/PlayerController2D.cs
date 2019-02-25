feb5a621505fedcca46565254cb167c6ad90dccb
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2019-02-25 21:08:50 +0100

    Added some sugar to input test by using Rotate/MoveTowards;

--- Assets/Game/Scripts/Runtime/PlayerController/PlayerController2D.cs
+++ Assets/Game/Scripts/Runtime/PlayerController/PlayerController2D.cs
@@ -8,13 +8,18 @@ namespace Wokarol.PlayerController
     public class PlayerController2D : MonoBehaviour
     {
         [SerializeField] InputData input = null;
+        private Vector3 targetScale = Vector3.one;
+        private Quaternion targetRotation;
 
         private void FixedUpdate() {
-            transform.rotation = Quaternion.Euler(0, 0, 10 * -input.Horizontal);
-            transform.localScale = new Vector3(
+            targetScale = new Vector3(
                 input.Jump ? 0.5f : 1f,
                 (input.Sliding ? 0.2f : 1f) * (input.Jump ? 1.2f : 1f)
             );
+            targetRotation = Quaternion.Euler(0, 0, 10 * -input.Horizontal);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 125f * Time.deltaTime);
+            transform.localScale = Vector3.MoveTowards(transform.localScale, targetScale, 10f * Time.deltaTime);
         }
     }
 }
